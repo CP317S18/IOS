1a7442138b8ec63ab25274d2e229737426ed7a5e
gregmurray <gd-murray@hotmail.com>
2018-07-05 18:18:54 -0400

    Storyboard layout and view specific classes

---
+++ shout/EnableBluetoothViewController.swift
@@ -0,0 +1,9 @@
+//
+//  EnableBluetoothViewController.swift
+//  shout
+//
+//  Created by Greg Murray on 2018-07-05.
+//  Copyright © 2018 wlu. All rights reserved.
+//
+
+import Foundation
---
+++ shout/SettingsViewController.swift
@@ -0,0 +1,9 @@
+//
+//  SettingsViewController.swift
+//  shout
+//
+//  Created by Greg Murray on 2018-07-05.
+//  Copyright © 2018 wlu. All rights reserved.
+//
+
+import Foundation
